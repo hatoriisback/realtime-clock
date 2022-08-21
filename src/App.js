ef969921be03f892cd33e3f8541cee5176dd9829
Hendy <hendy.findme@gmail.com>
2022-08-22 00:22:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      trigger1: "Tunggu ya bentar...",
+      trigger2: "",
+      trigger3: "",
+      nama: "",
+      // countdown: new Date(),
+      date: new Date(),
+    };
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date(),
+    });
+  };
+
+  // countdown = () => {
+  //   this.setState({
+  //     countdown: new Date(),
+  //   });
+  // };
+
+  greetTrig1 = () =>
+    this.setState({
+      trigger1: "",
+    });
+
+  greetTrig2 = () =>
+    this.setState({
+      trigger2: "Tunggu 5 detik lagi deh...",
+    });
+
+  greetTrig3 = () =>
+    this.setState({
+      trigger2: "",
+      trigger3: "Nungguin ya ? hehe... aku cuma mau bilang...",
+    });
+
+  greetTrig = () =>
+    this.setState({
+      trigger3: "",
+      nama: `I LOVE YOU FELICHIA!!! :))`,
+    });
+
+  componentDidMount() {
+    this.dateID = setInterval(() => this.tick(), 1000);
+    this.timerID = setInterval(() => this.tick(), 1000);
+    // this.countdownID = setTimeout(() => this.countdown(), 10000);
+    this.trigger1ID = setTimeout(() => this.greetTrig1(), 5000);
+    this.trigger2ID = setTimeout(() => this.greetTrig2(), 5001);
+    this.trigger3ID = setTimeout(() => this.greetTrig3(), 15002);
+
+    this.greetID = setTimeout(() => this.greetTrig(), 20003);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.dateID);
+    clearInterval(this.timerID);
+    // clearTimeout(this.countdownID);
+    clearInterval(this.trigger1ID);
+    clearInterval(this.trigger2ID);
+    clearInterval(this.trigger3ID);
+
+    clearInterval(this.greetID);
+  }
+
+  render() {
+    // let reduce = +this.state.countdown.toLocaleTimeString() - 10;
+    return (
+      <div className="App">
+        <div>
+          <h1>Realtime Clock</h1>
+          <hr />
+          <h2>Date: {this.state.date.toLocaleDateString()}</h2>
+          <h2>Time: {this.state.date.toLocaleTimeString()}</h2>
+          <h1>{this.state.trigger1}</h1>
+          <h1>{this.state.trigger2}</h1>
+          <h1>{this.state.trigger3}</h1>
+          <h1>{this.state.nama}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
